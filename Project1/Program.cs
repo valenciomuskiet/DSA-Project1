19f3f77e73e9278e202f6b1d391b63d3aa6a7c08
Valencio-Saex <valenciosaez.ict@gmail.com>
2026-02-26 14:39:47 +0100

    6. Program Entry Point: Wiring Up with Dependency Injection + 7. Running the Application

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Project1.Repository;
+using Project1.Service;
+using Project1.View;
+
+namespace Project1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Dependency injection: wiring up our components
+        string filePath = "tasks.json";
+
+        ITaskRepository repository = new JsonTaskRepository(filePath);
+        ITaskService service = new TaskService(repository);
+        ITaskView view = new ConsoleTaskView(service);
+
+        // Run the view
+        view.Run();
+    }
+}
