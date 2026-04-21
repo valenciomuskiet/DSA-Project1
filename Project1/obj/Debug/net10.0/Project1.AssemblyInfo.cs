9df169475797357fa13f7766e5e18189c22a5420
Valencio-Saex <valenciosaez.ict@gmail.com>
2026-04-21 13:10:22 +0200

    last commit

--- Project1/obj/Debug/net10.0/Project1.AssemblyInfo.cs
+++ Project1/obj/Debug/net10.0/Project1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dcf9db7bd29303f8d91dfcf0669494016cff36d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abbac63d8cc4e790400bb2715e7e166e3fad3d56")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
